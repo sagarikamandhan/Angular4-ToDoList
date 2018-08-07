c03fdcf26c40c40690c8a84077879e5f53812f35
sagarikamandhan <sagarikamandhan@gmail.com>
2018-08-08 01:11:45 +0530

    adding update and delete functionality

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -10,22 +10,50 @@ import{Task} from '../model/Tasks';
 })
 export class ToDoComponent implements OnInit {
     Items : Task[] =[];
-
+    added :Boolean;deleted : Boolean;
+    Message : String;DeleteMessage :String;
+    ErrorMessage: String;
+    NextID : Number;
   constructor(private dataservice : DataService) { }
   getAllTask(){
     this.dataservice.getAllTaskList().subscribe(items=>{
       this.Items.push(items);
+      this.NextID = this.Items.length+1;
       console.log(items);
     });
   }
-  addTask(form){
+  AddTask(form){
     let newTask : Task ={
-      id : this.Items.length+1,
+      id : this.NextID,
       title : form.value.taskname,
       completed : false
     }
-    this.Items.push(newTask);
+    if(form.value.taskname == ""){
+      this.ErrorMessage = "Please Enter task";
+    }
+    else{
+      this.ErrorMessage="";
+      console.log(form.value.taskname);
+      this.Items.push(newTask);
+      this.NextID =this.Items.length+1;
+      this.added = true;
+      this.Message ="Added successfully" ;
+      console.log(this.Items);
+    }
+
   }
+  DeleteTask(id){
+    this.Items.splice(id-1,1);
+    this.added =false;
+    this.deleted =true;
+    this.DeleteMessage = "Deleted successfully";
+  }
+  UpdateTask(task){
+    task.completed = !task.completed;
+    this.added =true;
+    this.deleted =false;
+    this.Message ="Updated successfully" ;
+      }
   ngOnInit() {
     this.getAllTask();
   }
